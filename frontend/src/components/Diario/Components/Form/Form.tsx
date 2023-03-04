a6848a01e1e0b835f947d0a710e523d72efea8b6
Felipe Botero <frpbotero@gmail.com>
2023-03-03 23:46:41 -0400

    update Form component
    Component style and structure update

--- frontend/src/components/Diario/Components/Form/Form.tsx
+++ frontend/src/components/Diario/Components/Form/Form.tsx
@@ -1,9 +1,11 @@
+import { FormContainer } from "./styles";
+
 export function Form(){
     return(
-        <>
+        <FormContainer>
             <input type="text" placeholder="Titúlo"/>
             <textarea placeholder="Três motivos para agradecer"/>
             <button>Salvar</button>
-        </>
+        </FormContainer>
     )
 }
---
+++ frontend/src/components/Diario/Components/Form/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FormContainer=styled.section`
+    display:flex;
+    flex-direction:column;
+    width:20vw;
+
+    textarea{
+        resize: none;
+    }
+`
