6a045d5709ec8279c9134c4be25213c6e1ccf475
Felipe Botero <frpbotero@gmail.com>
2023-03-03 21:48:43 -0400

    Update components Header and styles

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import profile from "../../assets/user16.svg"
+import { HeaderContainer } from "./styles"
+
 export function Header(){
     return(
 
-        <>
-        </>
+        <HeaderContainer>
+        <h1>Meu querido diario</h1>  
+        {/* Logo do projeto acima */}
+
+        <p>Felipe Botero</p>
+        <img src={profile} alt="" />
+        </HeaderContainer>
     )
 }
