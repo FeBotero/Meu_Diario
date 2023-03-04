70ece6162bcff84ba0adaca52bd81598158ab5e7
Felipe Botero <frpbotero@gmail.com>
2023-03-03 21:52:34 -0400

    Create component Form
    This component can receive the user's content diary

---
+++ frontend/src/components/Diario/Components/Form/Form.tsx
@@ -0,0 +1,9 @@
+export function Form(){
+    return(
+        <>
+            <input type="text" placeholder="Titúlo"/>
+            <textarea placeholder="Três motivos para agradecer"/>
+            <button>Salvar</button>
+        </>
+    )
+}
