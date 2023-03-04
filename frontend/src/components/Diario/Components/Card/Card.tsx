be9922f8ba87aa3dc13942f8251182702e614d3d
Felipe Botero <frpbotero@gmail.com>
2023-03-03 21:50:45 -0400

    Create component Card of Diario

---
+++ frontend/src/components/Diario/Components/Card/Card.tsx
@@ -0,0 +1,9 @@
+export function Card(){
+    return(
+        <>
+            <h2>Title</h2>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non illum corporis dolor temporibus eveniet accusantium quaerat voluptas, facilis autem. Ipsum non sapiente repellat laborum accusamus labore itaque exercitationem tempore ducimus?</p>
+        </>
+    )
+}
