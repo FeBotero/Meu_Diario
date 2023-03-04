db7fbc92c09d6d80b95175369b3e406f2a5b3fa8
Felipe Botero <frpbotero@gmail.com>
2023-03-03 23:59:48 -0400

    Update Config ContainerDiario

--- frontend/src/components/Diario/ContainerDiario.tsx
+++ frontend/src/components/Diario/ContainerDiario.tsx
@@ -1,13 +1,14 @@
 import { Card } from "./Components/Card/Card";
 import { Form } from "./Components/Form/Form";
+import { ContainerD } from "./styles";
 
 export function ContainerDiario(){
     return(
-        <>
+        <ContainerD>
             <Form/>
             <Card title="Hoje é dia" content="testando aplicação"/>
 
 
-        </>
+        </ContainerD>
     )
 }
---
+++ frontend/src/components/Diario/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContainerD = styled.div`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
