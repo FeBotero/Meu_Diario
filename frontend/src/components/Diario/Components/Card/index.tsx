328e005ee7fa53e6d9d687d8fa24daa013c0032f
Felipe Botero <frpbotero@gmail.com>
2023-03-11 10:19:34 -0400

    Inclui a verificação para exclusão de post apenas no dia atual

--- frontend/src/components/Diario/Components/Card/index.tsx
+++ frontend/src/components/Diario/Components/Card/index.tsx
@@ -1,12 +1,15 @@
 import { apiService } from "../../../../api/api";
 import { CardConteiner, Title } from "./styles";
 import { Trash} from "@phosphor-icons/react";
+import { dateContext } from "../../../../context/DateContext"
+import { useContext } from "react";
 
 export interface Icard{
     content:string
 }
 
 export function Card({content,idPost,refreshPost}:any){
+    const { date } = useContext(dateContext);
 
 
     async function deletePost() {
@@ -14,13 +17,16 @@ export function Card({content,idPost,refreshPost}:any){
         await apiService.post.deleteURL(idPost)
         refreshPost()
     }
-    
+    const getToday = new Date()
+    const today = JSON.stringify(Intl.DateTimeFormat('pt-BR').format(getToday))
+    const newDate = JSON.stringify(Intl.DateTimeFormat('pt-BR').format(date))
 
+    
 
     return(
         <CardConteiner>
             <Title>
-                <button onClick={deletePost}><Trash size={25}  /></button>
+            {newDate===today?<button onClick={deletePost}><Trash size={25}  /></button>:""}
             </Title>
             <p>{content}</p>
         </CardConteiner>
